eeb9f139f9cd26c7593c6a40a18e0a9eea22556f
Alan Robinson <71782048+KingNanachi@users.noreply.github.com>
2026-02-26 02:02:31 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool running = true;
+
+        while (running)
+        {
+            DisplayMenu();
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("Hello World!");
+                    break;
+
+                case "2":
+                    Console.Write("Enter a number: ");
+                    if (int.TryParse(Console.ReadLine(), out int number))
+                    {
+                        Console.WriteLine($"Result: {number * 2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid number.");
+                    }
+                    break;
+
+                case "3":
+                    running = false;
+                    Console.WriteLine("Goodbye World!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option.");
+                    break;
+            }
+        }
+    }
+
+    static void DisplayMenu()
+    {
+        Console.WriteLine("\n--- Main Menu ---");
+        Console.WriteLine("1. Say Hello World!");
+        Console.WriteLine("2. Multiply a number by 2");
+        Console.WriteLine("3. Exit");
+        Console.Write("Choose an option: ");
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# KachiMP3Balancer
+
